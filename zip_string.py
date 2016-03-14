1b6c03e93426067795a7607ab526cb1af58723df
moisesroth <moisesroth@gmail.com>
2016-03-14 09:42:54 -0300

    Update zip_string.py

--- zip_string.py
+++ zip_string.py
@@ -1,24 +1,24 @@
 def zip_string(s):
     r = ''
-    f = [ord(x) for x in s]
-    d = False
+    f = [ord(x) for x in s] # convert string in array of acii
+    d = False # dash flag
     
     for i in range(len(f)):
-        p = False
-        if i == 0 or i == len(f)-1:
+        p = False # print flag
+        if i == 0 or i == len(f)-1: # is first or last character
             p = True
-        elif abs(f[i-1] - f[i]) != 1:
+        elif abs(f[i-1] - f[i]) != 1: # diference between last letter and this letter ir != 1, this indicates non sequencial letter
             p = True
-        elif abs(f[i+1] - f[i]) != 1:
+        elif abs(f[i+1] - f[i]) != 1: # this verify non sequential letter
             p = True
-        elif f[i-1] == f[i+1]:
+        elif f[i-1] == f[i+1]: # verify if there is a change of sequential direction like "abcDcba"
             p = True
-        else:
+        else: # if no print was detected, print flag is seted to False and dash flag is seted to True
             p = False
             d = True
 
-        if p:
-            if d:
+        if p: 
+            if d: # if dash flag is True, this will make print a "-" before the next printed letter
                 r += '-'
             r += chr(f[i])
             d = False
